1c09dfbe30af595f085ecc886a73f3679f93b790
BenceOlay <olay.bence97@gmail.com>
2021-05-07 10:04:35 +0200

    Graph model added

---
+++ src/app/utils/graph.model.ts
@@ -0,0 +1,9 @@
+class Graph {
+    public name: string;
+    public description: string;
+
+    constructor(name: string, description: string) {
+        this.name = name;
+        this.description = description;
+    }
+}
