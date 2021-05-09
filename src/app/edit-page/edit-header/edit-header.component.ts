df3115a87d8eb738a3881c1b803fc6269aeed4b4
Mikkel Thomsen <mikkelbthomsen@icloud.com>
2021-05-09 14:37:52 +0200

    click handlers for menu items

--- src/app/edit-page/edit-header/edit-header.component.ts
+++ src/app/edit-page/edit-header/edit-header.component.ts
@@ -3,13 +3,44 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-edit-header',
   templateUrl: './edit-header.component.html',
-  styleUrls: ['./edit-header.component.css']
+  styleUrls: ['./edit-header.component.css'],
 })
 export class EditHeaderComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  handleOnAddObject() {
+    console.log('clicked on AddObject');
   }
-
+  handleOnRemoveObject() {
+    console.log('clicked on RemoveObject');
+  }
+  handleOnConnectObject() {
+    console.log('clicked on ConnectObject');
+  }
+  handleOnSaveAs() {
+    console.log('clicked on SaveAs');
+  }
+  handleOnLoad() {
+    console.log('clicked on Load');
+  }
+  handleOnSimulateGraph() {
+    console.log('clicked on SimulateGraph');
+  }
+  handleOnEditParameters() {
+    console.log('clicked on EditParameters');
+  }
+  handleOnViewExecutionLog() {
+    console.log('clicked on ViewExecutionLog');
+  }
+  handleOnAnalyzeGraph() {
+    console.log('clicked on AnalyzeGraph');
+  }
+  handleOnViewMetrics() {
+    console.log('clicked on ViewMetrics');
+  }
+  handleOnViewLogs() {
+    console.log('clicked on AddObject');
+  }
 }
