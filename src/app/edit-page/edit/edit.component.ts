51df62e555110c241b1b8dc18dce5d08ae792128
BenceOlay <olay.bence97@gmail.com>
2021-05-11 19:33:55 +0200

    Graph models improved
    Graph text view in progress 
    (Using SVG, a raw solution is added that need to be parameterised)
    
    Some little adjustments on the Front-Page
    
    Sytles.css - Added colored background (later can be an image)

--- src/app/edit-page/edit/edit.component.ts
+++ src/app/edit-page/edit/edit.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ResolveStart } from '@angular/router';
+import { Graph, Node, Role } from 'src/app/utils/graph.model';
 
 @Component({
   selector: 'app-edit',
@@ -7,7 +9,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EditComponent implements OnInit {
 
-  constructor() { }
+  graph: Graph = new Graph(0,"Sed vel ultrices","Mauris elit metus, posuere quis nisi a, sodales ornare odio.");
+  
+  constructor() {
+    this.graph.nodes = [
+      new Node(0,"Collect documents",0,null).setNextForMockUp(1),
+      new Node(1,"Unusual property",0,1).setNextForMockUp(2),
+      new Node(2,"Make appointment",1,1).setNextForMockUp(3),
+      new Node(3,"On-site appraisal",1,2).setNextForMockUp(4),
+      new Node(4,"Submit budget",2,3).setNextForMockUp(5),
+      new Node(5,"Approve budget",3,4).setNextForMockUp(6),
+      new Node(6,"Assess application",0,5).setNextForMockUp(null),
+    ];
+    this.graph.roles = [
+      new Role(0,"IT A"),
+      new Role(0,"IT B"),
+      new Role(0,"Client"),
+      new Role(0,"Intern"),
+    ];
+    console.log(this.graph);
+  }
 
   ngOnInit(): void {
   }
--- src/app/utils/graph.model.ts
+++ src/app/utils/graph.model.ts
@@ -1,18 +1,108 @@
 import { User } from "./user.model";
 
+/**
+ * A DCR Graph representation
+ * @public id : number
+ * @public name : string
+ * @public description : string
+ * @public shortDescription : string
+ * @public comments : Array(string)
+ * @public collaborators : Array(User)
+ * @public roles  : Array(Role)
+ * @public startRoles : number
+ * @public nodes : Array(Node)
+ */
 export class Graph {
-    public id: number;
+    public id: number; // Primary key
     public name: string;
     public description: string;
     public shortDescription: string;
     public comments: Array<string>;
     public collaborators: Array<User>;
 
+    public roles: Array<Role>;
+    public startRoles: number;
+    public nodes: Array<Node>; // Graph edges
+
     constructor(id: number,name: string, description: string) {
+        // Basic data
         this.id = id;
         this.name = name;
         this.description = description;
         this.shortDescription = description;
         this.comments = [];
+        this.collaborators = [];
+
+        // Graph inner data
+        this.startRoles = 0;
+        this.roles = [];
+        this.nodes = [];
+    }
+}
+
+/**
+ * This class is representing one role in DCR Graph
+ * @public id : number
+ * @public name : string
+ */
+export class Role {
+    public id: number; // Primary key
+    public name: string; // Role name
+
+    /**
+     * @param id Primary key
+     * @param name Name of the role
+     */
+    constructor(id: number, name: string) {
+        this.name = name;
+        this.id = id;
+    }
+}
+
+/**
+ * @public id: number
+ * @public roleID: number
+ * @public name: string
+ * @public prevID: number
+ * @public nextID: number
+ */
+export class Node {
+    public id: number; // Primary key
+    public roleID: number; // Role it's binded to
+    public name: string; // Description of the task
+    public prevID: number; // Previous task (might be null if it's the start)
+    public nextID: number; // Next task (TODO: make it an array, so conjunctions can be made - After the base solution is working)
+
+    /**
+     * The constructor
+     * @param id: primary key
+     * @param name: name of the task
+     * @param roleID: role it's bind to
+     * @param prevID: the previous task it comes from
+     */
+    constructor(id: number, name: string, roleID: number, prevID: number) {
+        this.name = name;
+        this.roleID = roleID;
+        this.id = id;
+        this.prevID = prevID;
+        this.nextID = null;
+    }
+
+    /**
+     * Set the next task
+     * @param id: number
+     */
+    setNextForMockUp(id: number) : Node {
+        this.nextID = id;
+        return this;
+    }
+
+    /**
+     * Set the next task
+     * @param id: number
+     */
+    setNext(id: number) {
+        this.nextID = id;
+        return this;
     }
 }
