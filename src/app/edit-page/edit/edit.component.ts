128cde0f8a99142de33dfe94b476da9c1ab8fb39
Mikkel Thomsen <mikkelbthomsen@icloud.com>
2021-05-13 08:41:24 +0200

    render different views

--- src/app/edit-page/edit/edit.component.ts
+++ src/app/edit-page/edit/edit.component.ts
@@ -1,15 +1,27 @@
 import { Component, OnInit } from '@angular/core';
 
+export type ViewType = 'text' | 'visual';
+
 @Component({
   selector: 'app-edit',
   templateUrl: './edit.component.html',
-  styleUrls: ['./edit.component.css']
+  styleUrls: ['./edit.component.css'],
 })
 export class EditComponent implements OnInit {
+  selectedViewType: ViewType = 'text';
 
-  constructor() { }
+  constructor() {}
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  handleOnChangeView(vt: ViewType) {
+    this.selectedViewType = vt;
   }
 
+  renderTextView(): boolean {
+    return this.selectedViewType === 'text';
+  }
+
+  renderVisualView(): boolean {
+    return this.selectedViewType === 'visual';
+  }
 }
