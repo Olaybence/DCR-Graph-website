68ae0a17da460de6505d140568e288c8ca4a77da
Mikkel Thomsen <mikkelbthomsen@icloud.com>
2021-05-13 08:38:54 +0200

    text view

---
+++ src/app/edit-page/text-view/text-view.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-text-view',
+  templateUrl: './text-view.component.html',
+  styleUrls: ['./text-view.component.css']
+})
+export class TextViewComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
