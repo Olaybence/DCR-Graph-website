91a86d54b85f17b647b603a404f37c56d0c96f71
Mikkel Thomsen <mikkelbthomsen@icloud.com>
2021-05-13 10:04:19 +0200

    basic implementation of the SEA2021 dcr graph

--- src/app/edit-page/text-view/text-view.component.ts
+++ src/app/edit-page/text-view/text-view.component.ts
@@ -1,15 +1,38 @@
 import { Component, OnInit } from '@angular/core';
 
+type Status = {
+  deadline: any;
+  enabled: boolean;
+  executed: boolean;
+  included: boolean;
+  label: string;
+  lastExecuted: any;
+  name: string;
+  pending: boolean;
+};
+
 @Component({
   selector: 'app-text-view',
   templateUrl: './text-view.component.html',
-  styleUrls: ['./text-view.component.css']
+  styleUrls: ['./text-view.component.css'],
 })
 export class TextViewComponent implements OnInit {
+  errorMsg: string;
+  graphStatus: Status[];
 
-  constructor() { }
+  constructor() {}
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  parse(input: string) {
+    this.errorMsg = '';
+
+    try {
+      const graph = window.parser.parse(input);
+      console.log(graph.status());
+      this.graphStatus = graph.status();
+    } catch (error) {
+      this.graphStatus = [];
+      this.errorMsg = error.message + JSON.stringify(error.location);
+    }
   }
-
 }
