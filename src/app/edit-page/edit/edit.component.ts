2e1f77b661454455237b0799b522f52671cd2c1b
Svaerken <btm676@alumni.ku.dk>
2021-05-13 14:28:30 +0200

    Edit id

--- src/app/edit-page/edit/edit.component.ts
+++ src/app/edit-page/edit/edit.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { NavigationEnd, Router } from '@angular/router';
 
 @Component({
   selector: 'app-edit',
@@ -6,10 +8,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./edit.component.css']
 })
 export class EditComponent implements OnInit {
+  edit_id: string;
 
-  constructor() { }
+
+  constructor(private router: Router) {
+    router.events.subscribe((evt) => {
+      if (evt instanceof NavigationEnd) {
+        this.edit_id = evt.url.split('/').pop();
+       console.log("I got it in .ts!!! " + this.edit_id);
+      }
+    })
+    }
+
+
 
   ngOnInit(): void {
+
   }
+
+
 
 }
