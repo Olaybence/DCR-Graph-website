5bdea73c9bcc4240e11557675b5b3e28d87a935a
MassiGaren <vpb466@alumni.ku.dk>
2021-05-28 17:17:24 +0200

    Front page done

--- src/app/edit-page/edit.module.ts
+++ src/app/edit-page/edit.module.ts
@@ -11,13 +11,15 @@ import { MatCheckboxModule } from '@angular/material/checkbox';
 import { GojsAngularModule } from 'gojs-angular';
 import { InspectorComponent } from './visual-view/inspector/inspector.component';
 import { FormsModule } from '@angular/forms';
+import { FooterComponent } from './footer/footer.component';
 @NgModule({
   declarations: [
     EditComponent,
     EditHeaderComponent,
     TextViewComponent,
     VisualViewComponent,
-    InspectorComponent
+    InspectorComponent,
+    FooterComponent
   ],
   imports: [
     CommonModule,
