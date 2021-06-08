553faa143d8b244c74028936060c70ad8d05d855
MassiGaren <vpb466@alumni.ku.dk>
2021-06-08 15:44:38 +0200

    Logs

--- src/app/edit-page/simulate/simulate.component.ts
+++ src/app/edit-page/simulate/simulate.component.ts
@@ -31,4 +31,15 @@ export class SimulateComponent implements OnInit {
   handleOnBack() {
     this.onBack.emit()
   }
+
+  array = ["Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."];
+
+  myFunction() {
+    var x = document.getElementById("myDIV");
+    if (x.innerHTML === " ") {
+      x.innerHTML = this.array[0];
+    } else {
+      x.innerHTML = " ";
+    }
+  }
 }
