5d599cf44fbb5a3c707a550f145c720a288aaea9
Svaerken <btm676@alumni.ku.dk>
2021-06-08 16:20:35 +0200

    Filter works
    Filter works with both local and shared projects. Sort is not working

--- src/sortParams.ts
+++ src/sortParams.ts
@@ -3,37 +3,40 @@ import {Directive,Output,EventEmitter,ElementRef,HostListener} from '@angular/co
 @Directive({selector: "[appSortParams]"})
 
 export class SortParamsDirective {
-@Output() param:EventEmitter<any>=new EventEmitter();
-constructor(private element:ElementRef) { }
+    @Output() param:EventEmitter<any>=new EventEmitter();
+    
+    constructor(private element:ElementRef) { }
 
-@HostListener('click') onClickIcon(){
-this.selectSort(this.element.nativeElement.id)
+    @HostListener('click') onClickIcon(){
+        this.selectSort(this.element.nativeElement.id)
+    }
+
+    selectSort(id){
+        switch(id){
+            case "firstAsc":
+                this.param.emit({dir:"asc",col:"first",typ:"string"})
+                break;
+            case "lastAsc":
+                this.param.emit({dir:"asc",col:"last",typ:"string"})
+                break;
+            case "ageAsc":
+                this.param.emit({dir:"asc",col:"age",typ:"number"})
+                break;
+            case "emailAsc":
+                this.param.emit({dir:"asc",col:"email",typ:"string"})
+                break;
+            case "firstDesc":
+                this.param.emit({dir:"desc",col:"first",typ:"string"})
+                break;
+            case "lastDesc":
+                this.param.emit({dir:"desc",col:"last",typ:"string"})
+                break;
+            case "ageDesc":
+                this.param.emit({dir:"desc",col:"age",typ:"number"})
+                break;
+            case "emailDesc":
+                this.param.emit({dir:"desc",col:"email",typ:"string"})
+                break;
+        }
+    }
 }
-
-selectSort(id){
-switch(id){
-case "firstAsc":
-this.param.emit({dir:"asc",col:"first",typ:"string"})
-break;
-case "lastAsc":
-this.param.emit({dir:"asc",col:"last",typ:"string"})
-break;
-case "ageAsc":
-this.param.emit({dir:"asc",col:"age",typ:"number"})
-break;
-case "emailAsc":
-this.param.emit({dir:"asc",col:"email",typ:"string"})
-break;
-case "firstDesc":
-this.param.emit({dir:"desc",col:"first",typ:"string"})
-break;
-case "lastDesc":
-this.param.emit({dir:"desc",col:"last",typ:"string"})
-break;
-case "ageDesc":
-this.param.emit({dir:"desc",col:"age",typ:"number"})
-break;
-case "emailDesc":
-this.param.emit({dir:"desc",col:"email",typ:"string"})
-break;
-}}}
